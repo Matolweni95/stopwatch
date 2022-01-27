b4ac2e55a5de5c14266e96068effcf5f2a72d3ac
masixolematolweni.1@gmail.com <72172920+Matolweni95@users.noreply.github.com>
2022-01-27 13:15:52 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stopwatch';
+
+
+  min: any = '00';
+  sec: any = '00';
+  milli: any = '00';
+
+  @Input() start!: boolean;
+  @Input() showTimerControls!: boolean;
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes['start']);
+    if (changes['start'].currentValue) {
+      this.startTimer();
+    }
+    else{
+      this.clearTimer();
+    }
+  }
+
+  counter!: number;
+  timerRef!: number;
+  running: boolean = false;
+  startText = 'Start';
+
+  startTimer() {
+    this.running = !this.running;
+    if (this.running) {
+      this.startText = 'Stop';
+      const startTime = Date.now() - (this.counter || 0);
+      this.timerRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+        this.milli = Math.floor(Math.floor(this.counter % 1000) / 10).toFixed(0);
+        this.min = Math.floor(this.counter / 60000);
+        this.sec = Math.floor(Math.floor(this.counter % 60000) / 1000).toFixed(0);
+      
+        if (Number(this.min) < 10) {
+          this.min = '0' + this.min;
+        } else {
+          this.min = '' + this.min;
+        }
+        if (Number(this.milli) < 10) {
+          this.milli = '0' + this.milli;
+        } else {
+          this.milli = '' + this.milli;
+        }
+        if (Number(this.sec) < 10) {
+          this.sec = '0' + this.sec;
+        } else {
+          this.sec = '' + this.sec;
+        }
+      });
+    } else {
+      this.startText = 'Resume';
+      clearInterval(this.timerRef);
+    }
+  }
+
+
+  clearTimer() {
+    this.running = false;
+    this.startText = 'Start';
+    this.counter = 0;
+    this.milli = '00',
+    this.sec = '00',
+    this.min = '00';
+    clearInterval(this.timerRef);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
+  }
+  ngOnInit() {
+  }
+
 }
